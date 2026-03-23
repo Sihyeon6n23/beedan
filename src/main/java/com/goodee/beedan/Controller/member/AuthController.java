5503d48823ecea7d9edc9c5285b80cb4a1ac7159
goheekwon <goheekwon100@naver.com>
2026-03-23 17:15:22 +0900

    viewPage

--- src/main/java/com/goodee/beedan/Controller/member/AuthController.java
+++ src/main/java/com/goodee/beedan/Controller/member/AuthController.java
@@ -25,7 +25,7 @@ public class AuthController {
         return "/member/auth/signin";
     }
 
-    @PostMapping("/signup")
+    @PostMapping("/signin")
     public String postSignin() {
         return "redirect:/auth/signin";
     }
