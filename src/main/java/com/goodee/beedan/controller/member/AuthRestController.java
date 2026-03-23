314579e64c7c785262977d303ee6cf27e9f2f128
goheekwon <goheekwon100@naver.com>
2026-03-23 23:40:38 +0900

    layout edit

--- src/main/java/com/goodee/beedan/controller/member/AuthRestController.java
+++ src/main/java/com/goodee/beedan/controller/member/AuthRestController.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthRestController {
-    /*@PostMapping("/biz-validation")
-    public ResponseEntity<BizResponseDto> postBizValidation (
-            @RequestBody BizRequestDto requestDto
-    ) {
-        BizResponseDto response = BizValidationService.validate();
-        return ResponseEntity.ok(response);
-    }*/
+    /*
+     * @PostMapping("/biz-validation")
+     * public ResponseEntity<BizResponseDto> postBizValidation (
+     * 
+     * @RequestBody BizRequestDto requestDto
+     * ) {
+     * BizResponseDto response = BizValidationService.validate();
+     * return ResponseEntity.ok(response);
+     * }
+     */
 }
-
-
