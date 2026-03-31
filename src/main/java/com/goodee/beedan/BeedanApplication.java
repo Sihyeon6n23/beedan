4c6e6ea0fa4e535cf5c60dbb5848fde746955659
JoonHee Choi <gameboy960501@gmail.com>
2026-04-01 00:02:12 +0900

    auto crawling by scheduler and button

--- src/main/java/com/goodee/beedan/BeedanApplication.java
+++ src/main/java/com/goodee/beedan/BeedanApplication.java
@@ -3,8 +3,10 @@ package com.goodee.beedan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BeedanApplication {
 
 	public static void main(String[] args) {
