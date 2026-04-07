27abcfc736a293cbca0b0cf0b655b6bca40658cd
goheekwon <goheekwon100@naver.com>
2026-04-07 21:05:22 +0900

    소셜로그인완료

--- src/main/java/com/goodee/beedan/config/security/MemberUserDetails.java
+++ src/main/java/com/goodee/beedan/config/security/MemberUserDetails.java
@@ -7,13 +7,17 @@ import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 @Data
-public class MemberUserDetails implements UserDetails {
+public class MemberUserDetails implements UserDetails, OidcUser {
     private String username;
     private String password;
     private List<SimpleGrantedAuthority> authorities;
@@ -25,7 +29,7 @@ public class MemberUserDetails implements UserDetails {
     private Long loginTryCount;
     private LocalDateTime accountLockExpirationTime;
 
-    public MemberUserDetails (Member member) {
+    public MemberUserDetails (Member member, OidcUser oidcUser) {
         this.username = member.getMemLgnId();
         this.displayName = member.getMemNm(); // 그 대신 displayName으로 회원의 이름을 저장
         this.bizName = member.getMemBizTtl(); // 사업자명 추가
@@ -58,5 +62,31 @@ public class MemberUserDetails implements UserDetails {
             return true;
         }
         return this.accountStatus.equals(MemberStatus.ACTIVE.toString());
+    }
+
+    // oidcUser 어쩔 수 없이 오버라이딩만 실제 미사용
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public Map<String, Object> getClaims() {
+        return Map.of();
+    }
+
+    @Override
+    public OidcUserInfo getUserInfo() {
+        return null;
+    }
+
+    @Override
+    public OidcIdToken getIdToken() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Map.of();
     }
 }
