d1e382144c65f00c16dbde8039df793ae4f73981
Cotowook <cotowook@naver.com>
2026-04-13 05:17:04 +0900

    Unipass 스케줄러, service 수정중

--- src/main/generated/com/goodee/beedan/mapper/board/BoardMapperImpl.java
+++ src/main/generated/com/goodee/beedan/mapper/board/BoardMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-04-13T04:08:04+0900",
+    date = "2026-04-13T05:15:41+0900",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.8 (Oracle Corporation)"
 )
 @Component
