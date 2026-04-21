d36d6c5864e4a32d61ef9771ff960a44bde3fb78
cotowook <cotowook@naver.com>
2026-04-21 16:18:27 +0900

    불필요한 controller 코드 제거

--- src/main/java/com/goodee/beedan/controller/order/OrderController.java
+++ src/main/java/com/goodee/beedan/controller/order/OrderController.java
@@ -1,53 +1,25 @@
 package com.goodee.beedan.controller.order;
 
-import com.goodee.beedan.common.constant.NotificationType;
-import com.goodee.beedan.config.security.MemberUserDetails;
-import com.goodee.beedan.dto.order.OrderDto;
-import com.goodee.beedan.repository.order.OrderRepository;
-import com.goodee.beedan.service.notification.NotificationService;
 import com.goodee.beedan.service.order.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/order")
 @RequiredArgsConstructor
 @Slf4j
 public class OrderController {
-    private final OrderRepository orderRepository;
     private final OrderService orderService;
-    private final NotificationService notificationService;
 
     @GetMapping("/list")
-    public String orderList(@AuthenticationPrincipal MemberUserDetails userDetails,
-                            @RequestParam(value = "status", defaultValue = "ALL") String status,
-                            @RequestParam(required = false) String keyword,
-                            @PageableDefault(size = 10, sort = "ordBaseCreDt", direction = Sort.Direction.DESC) Pageable pageable,
-                            Model model) {
-        Long memId = userDetails.getMemberId();
-
-        Page<OrderDto> orders = orderService.getOrderList(memId, status, keyword, pageable);
-        model.addAttribute("orders", orders);
-
+    public String orderList() {
         return "order/order-list";
     }
 
     @GetMapping("/detail")
-    public String orderDetail(@RequestParam("id") Long ordId,
-                              @AuthenticationPrincipal MemberUserDetails userDetails,
-                              Model model) {
-        OrderDto order = orderService.getOrderDetail(ordId, userDetails.getMemberId());
-        model.addAttribute("order", order);
-
+    public String orderDetail() {
         return "order/order-detail";
     }
 
